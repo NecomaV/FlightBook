9d0becf944e49336c3e817751e098c257bcd01a5
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-04-18 11:44:09 +0600

    img and layout

--- FlightBook/views.py
+++ FlightBook/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+
+from datetime import datetime, timedelta
+import calendar
+import math
+import secrets
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'FlightBook/layout.html')
+    min_date = f"{datetime.now().date().year}-{datetime.now().date().month}-{datetime.now().date().day}"
+    max_date = f"{datetime.now().date().year if (datetime.now().date().month+3)<=12 else datetime.now().date().year+1}-{(datetime.now().date().month + 3) if (datetime.now().date().month+3)<=12 else (datetime.now().date().month+3-12)}-{datetime.now().date().day}"
+    if request.method == 'POST':
+        origin = request.POST.get('Origin')
+        destination = request.POST.get('Destination')
+        depart_date = request.POST.get('DepartDate')
+        seat = request.POST.get('SeatClass')
+        trip_type = request.POST.get('TripType')
+        if(trip_type == '1'):
+            return render(request, 'flight/index.html', {
+            'origin': origin,
+            'destination': destination,
+            'depart_date': depart_date,
+            'seat': seat.lower(),
+            'trip_type': trip_type
+        })
+        elif(trip_type == '2'):
+            return_date = request.POST.get('ReturnDate')
+            return render(request, 'flight/index.html', {
+            'min_date': min_date,
+            'max_date': max_date,
+            'origin': origin,
+            'destination': destination,
+            'depart_date': depart_date,
+            'seat': seat.lower(),
+            'trip_type': trip_type,
+            'return_date': return_date
+        })
+    else:
+        return render(request, 'FlightBook/index.html', {
+            'min_date': min_date,
+            'max_date': max_date
+        })
